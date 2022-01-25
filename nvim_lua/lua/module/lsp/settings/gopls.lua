38dd169ebb251005bc9a77ca094426612746baca
Max.JKFC <maxjkfc@gmail.com>
2022-01-25 19:41:27 +0800

    add lsp

--- nvim_lua/lua/module/lsp/settings/gopls.lua
+++ nvim_lua/lua/module/lsp/settings/gopls.lua
@@ -1,5 +1,7 @@
 return {
     cmd = { "gopls" , "serve"},
+    filetype = { "go", "gomod"},
+    root_dir = require('lspconfig').util.root_pattern('go.mod','.git'),
     setting = {
         gopls = {
         analyses = {
