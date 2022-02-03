d7007d0c89800bf3bc44ac590166a6d10497fbb4
Max.JKFC <maxjkfc@gmail.com>
2022-02-03 21:05:02 +0800

    fix gopls remote failed

--- nvim_lua/lua/module/telescope.lua
+++ nvim_lua/lua/module/telescope.lua
@@ -7,11 +7,45 @@ local actions = require("telescope.actions")
 
 telescope.setup({
 	defaults = {
-
 		prompt_prefix = " ",
 		selection_caret = " ",
 		path_display = { "smart" },
-
+        vimgrep_arguments = {
+             "rg",
+             "--color=never",
+             "--no-heading",
+             "--with-filename",
+             "--line-number",
+             "--column",
+             "--smart-case",
+        },
+        layout_config = {
+             horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+             },
+             vertical = {
+                mirror = false,
+             },
+             width = 0.87,
+             height = 0.80,
+             preview_cutoff = 120,
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = { "node_modules" },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+        use_less = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
 		mappings = {
 			i = {
 				["<C-n>"] = actions.cycle_history_next,
--- nvim_lua/lua/module/which-key.lua
+++ nvim_lua/lua/module/which-key.lua
@@ -180,6 +180,7 @@ local mappings = {
 		u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
 		t = { "<cmd>lua _GOTOP_TOGGLE()<cr>", "Top" },
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+		g = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Git" },
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
