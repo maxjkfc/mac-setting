d34bff8e5c6ef1aca280c6f8195a60bb9b599f2d
maxjkfc <max210401@tyr-tech.com>
2022-09-26 15:06:35 +0800

    feat(custom): 更新tmux配置與cmp配置

--- nvim/lua/module/lsp/null-ls.lua
+++ nvim/lua/module/lsp/null-ls.lua
@@ -16,6 +16,7 @@ null_ls.setup({
     debug = false,
     sources = {
         formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.protolint,
         -- formatting.black.with({ extra_args = { "--fast" } }),
         -- formatting.stylua,
         diagnostics.flake8,
--- nvim/lua/module/lsp/settings/yamlls.lua
+++ nvim/lua/module/lsp/settings/yamlls.lua
@@ -1,12 +1,11 @@
-
 return {
     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
     settings = {
         yaml = {
             schemas = {
-                ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"]= "conf/**/*catalog*",
+                ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"] = "conf/**/*catalog*",
                 ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-                ["kubernetes"] = "/deploy.yml"
+                ["kubernetes"] = "/*.yml"
             }
         }
     }
