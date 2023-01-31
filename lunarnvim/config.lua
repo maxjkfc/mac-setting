3f3d70e5427c95019be95a440b1663ea252f26eb
maxjkfc <max210401@tyr-tech.com>
2023-01-31 16:15:37 +0800

    add gh.nvim plugins

--- lunarnvim/config.lua
+++ lunarnvim/config.lua
@@ -15,6 +15,9 @@ lvim.format_on_save = {
 	-- pattern = "*.lua",
 	timeout = 1000,
 }
+
+-- vim.env.PATH = "/opt/homebrew/opt/fzf" .. vim.env.PATH
+
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -27,8 +30,9 @@ lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<S-x"] = ":BufferKill<CR>"
 
-lvim.keys.normal_mode["[d"] = "<cmd>lua vim.diagnostic.goto_prev({ border = 'rounded' })<CR>"
-lvim.keys.normal_mode["]d"] = "<cmd>lua vim.diagnostic.goto_next({ border = 'rounded' })<CR>"
+lvim.keys.normal_mode["[d"] = ":TroubleToggle document_diagnostics<CR>"
+lvim.keys.normal_mode["]d"] = ":TroubleToggle workspace_diagnostics<CR>"
+-- lvim.keys.normal_mode["]d"] = "<cmd>lua vim.diagnostic.goto_next({ border = 'rounded' })<CR>"
 
 lvim.lsp.buffer_mappings.normal_mode["gi"] = lvim.lsp.buffer_mappings.normal_mode["gI"]
 lvim.builtin.gitsigns.opts.current_line_blame = false
@@ -79,9 +83,54 @@ lvim.builtin.which_key.mappings["t"] = {
 	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
 	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 }
+lvim.builtin.which_key.mappings["gh"] = {
+	name = "+Github",
+	c = {
+		name = "+Commits",
+		c = { "<cmd>GHCloseCommit<cr>", "Close" },
+		e = { "<cmd>GHExpandCommit<cr>", "Expand" },
+		o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
+		p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
+		z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
+	},
+	i = {
+		name = "+Issues",
+		p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
+	},
+	l = {
+		name = "+Litee",
+		t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
+	},
+	r = {
+		name = "+Review",
+		b = { "<cmd>GHStartReview<cr>", "Begin" },
+		c = { "<cmd>GHCloseReview<cr>", "Close" },
+		d = { "<cmd>GHDeleteReview<cr>", "Delete" },
+		e = { "<cmd>GHExpandReview<cr>", "Expand" },
+		s = { "<cmd>GHSubmitReview<cr>", "Submit" },
+		z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
+	},
+	p = {
+		name = "+Pull Request",
+		c = { "<cmd>GHClosePR<cr>", "Close" },
+		d = { "<cmd>GHPRDetails<cr>", "Details" },
+		e = { "<cmd>GHExpandPR<cr>", "Expand" },
+		o = { "<cmd>GHOpenPR<cr>", "Open" },
+		p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
+		r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
+		t = { "<cmd>GHOpenToPR<cr>", "Open To" },
+		z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
+	},
+	t = {
+		name = "+Threads",
+		c = { "<cmd>GHCreateThread<cr>", "Create" },
+		n = { "<cmd>GHNextThread<cr>", "Next" },
+		t = { "<cmd>GHToggleThread<cr>", "Toggle" },
+	},
+}
 
 -- -- Change theme settings
-lvim.colorscheme = "lunar"
+lvim.colorscheme = "tokyonight-night"
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerSync
 lvim.builtin.alpha.active = true
@@ -99,7 +148,6 @@ lvim.builtin.treesitter.highlight.enable = true
 -- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
 
 -- --- disable automatic installation of servers
-lvim.lsp.installer.setup.automatic_installation = true
 -- lvim.lsp.installer.setup.automatic_installation = false
 
 -- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
@@ -139,6 +187,12 @@ formatters.setup({
 		-- extra_args = { "-format", "-rm-unused", "-output", "file" },
 		filetypes = { "go", "go.mod" },
 	},
+	{
+		command = "buf",
+		-- command = "goimports-reviser",
+		-- extra_args = { "-format", "-rm-unused", "-output", "file" },
+		filetypes = { "proto" },
+	},
 })
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
@@ -150,6 +204,10 @@ linters.setup({
 	{
 		command = "revive",
 		filetypes = { "go", "go.mod" },
+	},
+	{
+		command = "buf",
+		filetypes = { "proto" },
 	},
 })
 
@@ -238,18 +296,9 @@ lvim.plugins = {
 	},
 	{
 		"nvim-telescope/telescope-fzy-native.nvim",
-		build = "make",
+		-- build = "make",
 		event = "BufRead",
 	},
-	-- {
-	-- 	-- 顯示目前此行Git 紀錄
-	-- 	"f-person/git-blame.nvim",
-	-- 	event = "BufRead",
-	-- 	config = function()
-	-- 		vim.cmd("highlight default link gitblame SpecialComment")
-	-- 		vim.g.gitblame_enabled = 0
-	-- 	end,
-	-- },
 	{
 		-- Golang 擴充套件
 		"ray-x/go.nvim",
@@ -299,6 +348,56 @@ lvim.plugins = {
 		"kylechui/nvim-surround",
 		config = function()
 			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"ldelossa/gh.nvim",
+		dependencies = { { "ldelossa/litee.nvim" } },
+		config = function()
+			require("litee.lib").setup()
+			require("litee.gh").setup({
+				-- deprecated, around for compatability for now.
+				jump_mode = "invoking",
+				-- remap the arrow keys to resize any litee.nvim windows.
+				map_resize_keys = false,
+				-- do not map any keys inside any gh.nvim buffers.
+				disable_keymaps = false,
+				-- the icon set to use.
+				icon_set = "default",
+				-- any custom icons to use.
+				icon_set_custom = nil,
+				-- whether to register the @username and #issue_number omnifunc completion
+				-- in buffers which start with .git/
+				git_buffer_completion = true,
+				-- defines keymaps in gh.nvim buffers.
+				keymaps = {
+					-- when inside a gh.nvim panel, this key will open a node if it has
+					-- any futher functionality. for example, hitting <CR> on a commit node
+					-- will open the commit's changed files in a new gh.nvim panel.
+					open = "<CR>",
+					-- when inside a gh.nvim panel, expand a collapsed node
+					expand = "zo",
+					-- when inside a gh.nvim panel, collpased and expanded node
+					collapse = "zc",
+					-- when cursor is over a "#1234" formatted issue or PR, open its details
+					-- and comments in a new tab.
+					goto_issue = "gd",
+					-- show any details about a node, typically, this reveals commit messages
+					-- and submitted review bodys.
+					details = "d",
+					-- inside a convo buffer, submit a comment
+					submit_comment = "<C-s>",
+					-- inside a convo buffer, when your cursor is ontop of a comment, open
+					-- up a set of actions that can be performed.
+					actions = "<C-a>",
+					-- inside a thread convo buffer, resolve the thread.
+					resolve_thread = "<C-r>",
+					-- inside a gh.nvim panel, if possible, open the node's web URL in your
+					-- browser. useful particularily for digging into external failed CI
+					-- checks.
+					goto_web = "gx",
+				},
+			})
 		end,
 	},
 }
